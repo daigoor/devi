903e499bd52822fc47bcb0d3b648fcc708698425
DaiGooR <daigoor@gmail.com>
2019-07-24 21:47:48 +0300

    Working version

--- private/controls/camera.py
+++ private/controls/camera.py
@@ -8,15 +8,11 @@ import sys
 import RPi.GPIO as GPIO
 import time
 
-servoPINUD = 22
-servoPINLR = 26
+servoPINUD = 15
+servoPINLR = 37
 
-step = 1
 PWMHz = 50
 pwds=7.5
-
-file_lr = 'lrp.d'
-file_ud = 'udp.d'
 
 
 def main():
@@ -43,67 +39,43 @@ def main():
 
 def turn_up():
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(servoPINUD, GPIO.OUT)
     GPIO.setup(servoPINLR, GPIO.OUT)
     pwm = GPIO.PWM(servoPINUD, PWMHz) # GPIO 17 for PWM with 50Hz
-    position = _read_file(file_ud)
     pwm.start(pwds) # Initialization
-    new_position = float(position) + float(step)
     pwm.ChangeDutyCycle(12.5)
-    _write_file(file_ud, new_position)
 
 def turn_down():
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(servoPINUD, GPIO.OUT)
     GPIO.setup(servoPINLR, GPIO.OUT)
     pwm = GPIO.PWM(servoPINUD, PWMHz) # GPIO 17 for PWM with 50Hz
-    position = _read_file(file_ud)
     pwm.start(pwds) # Initialization
-    new_position = float(position) - float(step)
     pwm.ChangeDutyCycle(2.5)
-    _write_file(file_ud, new_position)
 
 def turn_right():
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(servoPINUD, GPIO.OUT)
     GPIO.setup(servoPINLR, GPIO.OUT)
     pwm = GPIO.PWM(servoPINLR, PWMHz) # GPIO 17 for PWM with 50Hz
-    position = _read_file(file_lr)
     pwm.start(pwds) # Initialization
-    new_position = float(position) + float(step)
     pwm.ChangeDutyCycle(12.5)
-    _write_file(file_lr, new_position)
 
 def turn_left():
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(servoPINUD, GPIO.OUT)
     GPIO.setup(servoPINLR, GPIO.OUT)
     pwm = GPIO.PWM(servoPINLR, PWMHz) # GPIO 17 for PWM with 50Hz
-    position = _read_file(file_lr)
     pwm.start(pwds) # Initialization
-    new_position = float(position) - float(step)
     pwm.ChangeDutyCycle(4)
-    _write_file(file_lr, new_position)
 
 def _stop():
     """STOP ALL KIND OF MOVEMENTS"""
 
-def _read_file(path):
-    #file = open(path,'rb') # open file , r => read , b => byte format
-    #value = file.read()
-    #file.close()
-    #print(float(value))
-    return float(1)
-
-def _write_file(path, value):
-    print(float(value))
-    #file = open(path,'w') # open file , r => read , b => byte format
-    #file.write(str(value)) 
-    #file.close()
 
 if __name__ == "__main__":
     main()
--- private/server.py
+++ private/server.py
@@ -6,16 +6,149 @@ __copyright__   = "Copyright 2019, DaiGooR"
 
 import io
 import logging
+import picamera
 import socketserver
+import constants
 from threading import Condition
 from http import server
+from control import Vehicle, Camera
 
-from route import StreamingHandler
+vehicle_controls = Vehicle()
+camera_controls = Camera()
 
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self._redirect()
+        elif self.path.endswith('.ico'):
+            content = self._read_file('../Favicon.ico')
+            self._set_common_headers(constants.CONTENT_TYPE_JPEG, len(content))
+            self.wfile.write(content)
+        elif self.path.endswith('.html'):
+            self.open_html()
+        elif self.path.endswith('.css'):
+            self.load_css()
+        elif self.path.endswith('.js'):
+            self.load_js()
+        elif self.path == '/stream.mjpg':
+            self.stream_camera()
+        elif self.path.startswith('/robot?'):
+            self.move_vehicle()
+        elif self.path.startswith('/camera?'):
+            self.turn_camera()
+        else:
+            self._return_error()
+
+    def move_vehicle(self):
+        direction, movement, key = self._extract_param()
+        print('direction -> ' + direction)
+        print('movement -> ' + movement)
+        print('key -> ' + key)
+        vehicle_controls.move(direction, movement)
+        self._set_common_headers('', len(''))
+
+    def turn_camera(self):
+        direction, movement, key = self._extract_param()
+        print('direction -> ' + direction)
+        print('movement -> ' + movement)
+        print('key -> ' + key)
+        camera_controls.turn(direction, movement)
+        self._set_common_headers('', len(''))
+
+    def load_js(self):
+        content = self._read_file(constants.DIR_JS + self.path)
+        self._set_common_headers(constants.CONTENT_TYPE_JS, len(content))
+        self.wfile.write(content)
+    
+    def load_css(self):
+        content = self._read_file(constants.DIR_CSS + self.path)
+        self._set_common_headers(constants.CONTENT_TYPE_CSS, len(content))
+        self.wfile.write(content)
+
+    def open_html(self):
+        content = self._read_file(constants.DIR_HTML + self.path)
+        self._set_common_headers(constants.CONTENT_TYPE_HTML, len(content))
+        self.wfile.write(content)
+
+    def stream_camera(self):
+        self.send_response(200)
+        self.send_header('Age', 0)
+        self.send_header('Cache-Control', 'no-cache, private')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header(constants.HEADER_CONTENT_TYPE, constants.CONTENT_TYPE_MULTIPART)
+        self.end_headers()
+       
+        try:
+            while True :
+                with output.condition:
+                     output.condition.wait()
+                     frame = output.frame
+                self.wfile.write(b'--FRAME\r\n')
+                self.send_header(constants.HEADER_CONTENT_TYPE, constants.CONTENT_TYPE_JPEG)
+                self.send_header(constants.HEADER_CONTENT_LENGTH, len(frame))
+                self.end_headers()
+                self.wfile.write(frame)
+                self.wfile.write(b'\r\n')
+        except Exception as e:
+            logging.warning('Removed streaming client %s: %s',self.client_address, str(e))
+
+    def _redirect(self):
+        self.send_response(301)
+        self.send_header('Location', '/index.html')
+        self.end_headers()
+
+    def _read_file(self, path):
+        file = open(path,'rb') # open file , r => read , b => byte format
+        content = file.read()
+        file.close()
+        return content
+
+    def _set_common_headers(self, contentType, contentLength):
+        self.send_response(200)
+        self.send_header(constants.HEADER_CONTENT_TYPE, contentType)
+        self.send_header(constants.HEADER_CONTENT_LENGTH, contentLength)
+        self.end_headers()
+    
+    def _extract_param(self):
+        parametersStr = self.path.split('?')
+        parametersList = parametersStr[1].split('&')
+        direction = parametersList[0].split('=')[1]
+        movement = parametersList[1].split('=')[1]
+        key = parametersList[2].split('=')[1]
+        return direction, movement, key
+
+    def _return_error(self):
+        self.send_error(404)
+        self.end_headers()
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-address = ('', 8000)
-server = StreamingServer(address, StreamingHandler)
-server.serve_forever()
+with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+    output = StreamingOutput()
+    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+    camera.rotation = 180
+    camera.start_recording(output, format='mjpeg')
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
